ff44c4ac55eb84c252b30ae70b7c3c3e716e6243
ubejtemena <q>
2025-02-05 14:09:10 +0100

    update front

--- src/script.js
+++ src/script.js
@@ -1,13 +1,18 @@
 const output = document.getElementById("output");
 const input = document.getElementById("input");
 const button = document.getElementById("button");
+const short = document.getElementById("short");
 
+window.addEventListener("load", ()=>{
+    short.style.display = "none";
+})
 
 async function Get(){
     const data = {
         url: input.value
     };
     input.value = "";
+    short.style.display = "block";  
     try {
         const res = await fetch('http://localhost:3000', {
             method: 'POST', 
