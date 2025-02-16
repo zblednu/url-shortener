c3b3fa88147613bcf8f2fb4a4330ac42d859b708
zblednu <maksym.surovtsev@student.tuke.sk>
2025-02-16 16:40:14 +0100

    restructure for containers

--- db-bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/sh
-sudo su postgres -c '
-	psql -c "CREATE ROLE shortener LOGIN";
-	psql -c "CREATE DATABASE shortener OWNER shortener";
-	psql -U shortener shortener -c "CREATE TABLE maps ( \
-		id char(8) PRIMARY KEY, \
-		url varchar(800) NOT NULL \
-	)";
-'
